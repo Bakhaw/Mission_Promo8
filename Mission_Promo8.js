d3c7b9233d4c6a0f60a63b46d4eb9ccc14f81d05
Felix Saint-Genis <saintgenisfelix@gmail.com>
2017-06-20 10:13:12 +0200

    add new features ouai ouai

--- Mission_Promo8.js
+++ Mission_Promo8.js
@@ -1,9 +1,12 @@
-var re = /choix/gi;
-var rere = /date/gi;
-var str = "Elle a le choix dans la date. ";
-var nouvelleStr = str.replace(re, "doigt");
-var suite = nouvelleStr.replace(rere, "chatte") + "En outre elle se trouve dans la rue du quai";
+var re = /camion/gi;
+var rere = /voiture/gi;
+var str = "Elle a le camion dans la voiture.";
+var nouvelleStr = str.replace(re, "vamion");
+var suite = nouvelleStr.replace(rere, "coiture") + "En outre elle se trouve dans la rue du quai";
 var rerere = /rue/gi;
 var rererere = /quai/gi;
-var toBeContinued = suite.replace(rerere, "raie").replace(rererere, "cul")
-toBeContinued
+var toBeContinued = suite.replace(rerere, "raie").replace(rererere, "cul");
+
+var toBeContinuedUppercase = toBeContinued.toUpperCase();
+
+console.log(toBeContinuedUppercase);
